d0409470a25422c4abfeee95e99976da6d214291
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-08-25 18:37:56 +0100

    :white_check_mark: completed the getting started section

---
+++ 01. Getting Started/hello_world/main.rs
@@ -0,0 +1,7 @@
+// The main function is always the first code that runs in every executable Rust program
+fn main() {
+    println!("Hello, world!"); // the ! indicates that a rust macro was called instead of a function
+}
+
+// Use the command: rustc main.rs to compile the program 
+// and ./main to run it  
