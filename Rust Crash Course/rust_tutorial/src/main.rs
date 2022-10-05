538770675ee765fc78770ebdba0070113d4e8ac0
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-05 12:45:21 +0100

    white_check_mark: completed up to enums

--- Rust Crash Course/rust_tutorial/src/main.rs
+++ Rust Crash Course/rust_tutorial/src/main.rs
@@ -67,5 +67,172 @@ fn main() {
     // let random_num = rand::thread_rng().gen_range(1..101);
     // println!("Random : {}", random_num);
 
-    //**** start from if expressions. */
+    /**** start from if expressions. */
+    // let age = 8;
+    // if (age >= 1) && (age <= 18) {
+    //     println!("Important Birthday");
+    // } else if (age == 21) || (age == 50) {
+    //     println!("Important Birthday level 2");
+    // } else if age >= 65 {
+    //     println!("Important Birthday level 3");
+    // } else {
+    //     println!("Not an Important Birthday")
+    // }
+
+    // let mut my_age = 47;
+    // let can_vote = if my_age >= 18 { true } else { false };
+    // println!("Can Vote : {}", can_vote);
+
+    /* Match Conditional */
+    // let age2 = 20;
+    // match age2 {
+    //     // if age2 match num in range 1 - 19
+    //     1..=18 => println!("Important Birthday"),
+
+    //     // if age2 match 21 or 50
+    //     21 | 50 => println!("Important Birthday level 2"), 
+
+    //     // if age2 match num in range 65 - MAX of i32
+    //     65..=i32::MAX => println!("Important Birthday level 3"),
+
+    //     // else
+    //     _ => println!("Not an important Birthday"),
+
+    // };
+
+    // println!("Just Checking");
+
+    // let my_age: i8 = 18;
+    // let voting_age: i8 = 18; 
+
+    // match my_age.cmp(&voting_age) {
+    //     Ordering::Less => println!("Can't vote"), 
+    //     Ordering::Greater => println!("Can Vote"), 
+    //     Ordering::Equal => println!("You gained the right to vote"),
+    // };
+
+    // println!("{}", voting_age);
+
+    /*** Arrays ****/
+//     let arr_1 = [1, 2, 3, 4,];
+//     println!("1st: {}", arr_1[0]);
+//     println!("Length: {}", arr_1.len());
+
+//     // looping through arrays 
+//     let arr_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//     let mut loop_idx = 0;
+
+//     // using loop
+//     loop {
+//         if arr_2[loop_idx] % 2 == 0 {
+//             loop_idx += 1;
+//             continue;
+//         }
+//          if arr_2[loop_idx]  == 9 {
+//            break;
+//         }
+//         println!("Val: {}", arr_2[loop_idx]);
+//         loop_idx  += 1;
+        
+//     }
+//    // using while loops 
+//    let mut loop_idx = 0;
+
+//    println!();
+//    while loop_idx < arr_2.len() {
+//     println!("Arr: {}", arr_2[loop_idx]);
+//     loop_idx += 1;
+//    } 
+
+//    // using for loops 
+//    println!();
+//    for val in arr_2.iter() {
+//     println!("Val: {}", val);
+//    }
+
+   /***** Tuples *******/
+//    let my_tuple: (u8, String, f64) = (47, "Omotoye".to_string(), 50_000.00);
+
+//    println!("Name: {}", my_tuple.1);
+
+//    // tuple unpacking 
+//    let (v1, v2, v3) = my_tuple;
+//    println!("Age: {}", v1);
+
+    /********* Strings *********/
+    // let mut st1: String = String::new();
+    // st1.push('A');
+    // st1.push_str(" word");
+    
+    // for word in st1.split_whitespace() {
+    //     println!("{}", word);
+    // }
+    // let st2 = st1.replace("A", "Another");
+    // println!("{}", st2);
+
+    // let st3 = String::from("x r t b k k a m c");
+    // let mut v1: Vec<char> = st3.chars().collect();
+    // v1.sort();
+    // v1.dedup();
+    // for char in v1 {
+    //     println!("{}", char);
+    // }
+
+    // // Creating a string by getting a reference to a string
+    // let st4: &str = "Random String";
+    // let mut st5: String = st4.to_string();
+    // println!("{}", st5);
+    // let byte_arr1 = st5.as_bytes();
+
+    // let st6 = &st5[3..9];
+    // println!("{}", st6);
+    // println!("String length: {}", st6.len());
+    // st5.clear();
+
+    // // combining strings 
+    // let st6: String = String::from("Just some");
+    // let st7: String = String::from(" words");
+    // let st8: String = st6 + &st7;
+    // for char in st8.bytes() {
+    //     println!("{}", char);
+    // }
+
+   /********* Casting ********/
+//    let int_u8: u8 = 5;
+//    let int2_u8: u8 = 4; 
+//    let int3_u32: u32 = (int_u8 as u32) + (int2_u8 as u32);
+
+    /******** Enums *************/
+    enum Day {
+        Monday, 
+        Tuesday,
+        Wednesday,
+        Thursday, 
+        Friday,
+        Saturday,
+        Sunday,
+    };
+
+    // Defining Methods for an Enum type
+    impl Day {
+        fn is_weekend($self) -> bool {
+            match self {
+                Day::Saturday | Day::Sunday => true, 
+                _ => false
+            };
+        }
+    }
+
+    let today: Day = Day::Monday;
+    match today {
+        Day::Monday => println!("Everyone hates Monday"), 
+        Day::Tuesday => println!("Donut day"),
+        Day::Wednesday => println!("Hump day"),
+        Day::Thursday => println!("Pay day"),
+        Day::Friday => println!("Almost Weekend"),
+        Day::Saturday => println!("Weekend"),
+        Day::Sunday => println!("Weekend"),
+    };
+
+    println!("Is today the weekend {}", today.is_weekend());
 }
