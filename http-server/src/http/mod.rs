d85ba053792aa9f5c4cae923f186ac3f5f4fc29a
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-09 16:20:16 +0100

    :construction: <not working> added more functionality to the server

--- http-server/src/http/mod.rs
+++ http-server/src/http/mod.rs
@@ -3,4 +3,5 @@ pub mod request;
 
 pub use request::Request;
 pub use method::Method;
+pub use request::ParseError;
 
--- http-server/src/server.rs
+++ http-server/src/server.rs
@@ -1,5 +1,7 @@
-use std::net::TcpListener;
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::io::Read;
+use std::net::TcpListener;
 pub struct Server {
     addr: String,
 }
@@ -15,15 +17,19 @@ impl Server {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
-        
-        loop {
 
+        loop {
             match listener.accept() {
                 Ok((mut stream, _)) => {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
-                        Ok (_) => {
+                        Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
                         }
                         Err(e) => {
                             println!("Failed to read from connection: {}", e);
@@ -31,7 +37,7 @@ impl Server {
                     }
                 }
                 Err(e) => {
-                   println!("Failed to establish a connection: {}", e);
+                    println!("Failed to establish a connection: {}", e);
                 }
             }
         }
