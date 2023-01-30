c794d52e0e96462a80271f92b85eaf6bdedff605
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2023-01-30 06:25:41 +0100

    :construction: <init> starting the rust book again with a fresh mind

--- comprehensive-rust-by-google/day_2/morning/exercises/src/main.rs
+++ comprehensive-rust-by-google/day_2/morning/exercises/src/main.rs
@@ -1,3 +1,26 @@
+// TODO: remove this when you're done with your implementation
+#![allow(unused_variables, dead_code)]
+
+struct User {
+    name: String,
+    age: u32,
+    weight: f32,
+}
+
+impl User {
+    pub fn new(name: String, age: u32, weight: f32) -> Self {
+        User {
+            name: name,
+            age: age,
+            weight: weight,
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    unimplemented!()
 }
